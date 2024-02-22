108b791ab752f3a0322942aafda59c18f4c51f11
Vincent Yang <vincentyangoz@gmail.com>
2024-02-23 02:17:41 +1100

    add test script for accepting one line only

---
+++ testModelOne.py
@@ -0,0 +1,70 @@
+import re
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import svm
+import joblib
+import sys
+from sep_char import separate_chinese_and_english 
+
+#english_pattern = re.compile(r'([a-zA-Z0-9]+)')     # English alphabet
+#english_pattern = re.compile(r'([a-zA-Z0-9\.\/\*\\]+)')         # English alphabet
+
+# Load the saved model from file
+def load_model(model_path):
+    try:
+        model = joblib.load(model_path)
+        return model
+    except Exception as e:
+        print("Error loading model:", e)
+        sys.exit(1)
+
+# Load the saved CountVectorizer from file
+def load_vectorizer(vectorizer_path):
+    try:
+        vectorizer = joblib.load(vectorizer_path)
+        return vectorizer
+    except Exception as e:
+        print("Error loading vectorizer:", e)
+        sys.exit(1)
+
+# Accept input from stdin and output the predicted label
+def predict_label(model, vectorizer):
+    while True:
+        try:
+            # Accept input from stdin
+            text = input("").strip()
+            if text.lower() == 'quit':
+                break
+
+            text = separate_chinese_and_english(text)
+
+            # Transform the input text using the CountVectorizer
+            features = vectorizer.transform([text])
+
+            # Predict the label
+            predicted_label = model.predict(features)[0]
+
+            # Output the predicted label
+            print(predicted_label)
+            break
+        except Exception as e:
+            print("Error:", e)
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python testModel.py <vectorizer_path> <model_path>")
+        sys.exit(1)
+
+    vectorizer_path = sys.argv[1]
+    model_path = sys.argv[2]
+
+    # Load the saved model and CountVectorizer from file
+    loaded_model = load_model(model_path)
+    loaded_vectorizer = load_vectorizer(vectorizer_path)
+
+    # Predict labels for input text
+    predict_label(loaded_model, loaded_vectorizer)
+
+if __name__ == "__main__":
+    main()
+
